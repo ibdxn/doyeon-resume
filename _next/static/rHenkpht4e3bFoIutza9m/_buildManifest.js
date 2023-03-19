51db55f32f01695a0e9403823e7b34a35cc7b2e8
Doyeon <ibdxn@naver.com>
2023-03-19 19:59:57 +0900

    Updates

---
+++ _next/static/rHenkpht4e3bFoIutza9m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b1f2e3a385ea0661.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
